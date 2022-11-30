396b46ed87068b2b16ed11f2f74db8567c7d3c6b
BelenArguello <119349486+BelenArguello@users.noreply.github.com>
2022-11-30 18:43:06 -0300

    Add files via upload

---
+++ libreria.py
@@ -0,0 +1,15 @@
+#import calendar
+#año=2022
+#mes=3
+#print(calendar.month(año,mes))
+
+
+#import calendar
+#año=2022
+#mes=5
+#print(calendar.month(año,mes))
+
+import calendar
+año=2022
+mes=int(input("Ingrese el mes: "))
+print(calendar.month(año,mes))
---
+++ textoavoz.py
@@ -0,0 +1,6 @@
+#libreria texto a voz
+import pyttsx3
+texto=pyttsx3.init
+texto.say("Hola mundo con python")
+texto.runAndwait()
+
