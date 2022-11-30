da361876c1e002c95c9a3c55cb9a960e20550bdb
BelenArguello <119349486+BelenArguello@users.noreply.github.com>
2022-11-30 18:29:31 -0300

    Add files via upload

---
+++ Ejercicio2.py
@@ -0,0 +1,22 @@
+
+
+
+n1= int(input("digite el primer numero: "))
+n2= int(input("digite el segundo numero: "))
+
+sum= n1+n2
+print("La suma es ")
+print(sum)
+
+rest= n1-n2
+print("La resta es ")
+print(rest)
+
+
+mult = n1*n2
+print("La multiplicacion es ")
+print(mult)
+
+div=n1//n2
+print("La division es")
+print(div)
